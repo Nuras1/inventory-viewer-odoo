94e9f0cd99deff0d2018a2b6b9dc0087a0130e59
Nuras1 <nurasbekmurad@mail.ru>
2026-06-01 18:33:32 +0500

    Configure

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM odoo:18
 
 COPY ./addons /mnt/extra-addons
+COPY ./odoo.conf /etc/odoo/odoo.conf
+
+CMD ["odoo","-c","/etc/odoo/odoo.conf"]
---
+++ addons/inventory_viewer/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Inventory Viewer",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/inventory_views.xml"
+    ],
+    "installable": True,
+    "application": True
+}
