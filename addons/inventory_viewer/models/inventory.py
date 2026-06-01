922d56492584de9581c0d5acf90953204266b11d
Nuras1 <nurasbekmurad@mail.ru>
2026-06-01 18:13:20 +0500

    Initial Odoo module

---
+++ addons/inventory_viewer/models/inventory.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class InventoryViewer(models.Model):
+    _name = "inventory.viewer"
+    _description = "Imported Inventory"
+
+    name = fields.Char(required=True)
+
+    category = fields.Char()
+
+    items_count = fields.Integer()
+
+    api_token = fields.Char()
