284fd44909b9ae56899f53abeba083fdd3530427
Nuras1 <nurasbekmurad@mail.ru>
2026-06-02 16:09:31 +0500

    Inventory import2

--- addons/inventory_viewer/models/import_wizard.py
+++ addons/inventory_viewer/models/import_wizard.py
@@ -4,27 +4,35 @@ import requests
 
 class InventoryImportWizard(models.TransientModel):
     _name = "inventory.import.wizard"
-    _description = "Import Inventory"
 
-    api_url = fields.Char(
-        required=True
-    )
+    api_url = fields.Char(required=True)
 
     def action_import(self):
 
-        response = requests.get(
-            self.api_url,
-            timeout=30
-        )
+        response = requests.get(self.api_url)
 
         data = response.json()
 
-        self.env["inventory.viewer"].create({
-            "name": data["title"],
-            "category": data["category"],
-            "items_count": data["itemsCount"]
+        inventory = self.env["inventory.viewer"].create({
+            "name": data.get("title"),
+            "category": data.get("category"),
+            "items_count": data.get("itemsCount"),
         })
 
-        return {
-            "type": "ir.actions.act_window_close"
-        }
+        for field in data.get("fields", []):
+
+            self.env["inventory.viewer.field"].create({
+                "inventory_id": inventory.id,
+                "name": field.get("name"),
+                "field_type": field.get("type")
+            })
+
+        for aggregate in data.get("aggregates", []):
+
+            self.env["inventory.viewer.aggregate"].create({
+                "inventory_id": inventory.id,
+                "field_name": aggregate.get("field"),
+                "value": str(aggregate)
+            })
+
+        return True
--- addons/inventory_viewer/models/inventory.py
+++ addons/inventory_viewer/models/inventory.py
@@ -3,10 +3,40 @@ from odoo import models, fields
 
 class InventoryViewer(models.Model):
     _name = "inventory.viewer"
-    _description = "Imported Inventory"
 
     name = fields.Char(required=True)
-
     category = fields.Char()
-
     items_count = fields.Integer()
+    api_token = fields.Char()
+
+    field_ids = fields.One2many(
+        "inventory.viewer.field",
+        "inventory_id"
+    )
+
+    aggregate_ids = fields.One2many(
+        "inventory.viewer.aggregate",
+        "inventory_id"
+    )
+
+
+class InventoryViewerField(models.Model):
+    _name = "inventory.viewer.field"
+
+    inventory_id = fields.Many2one(
+        "inventory.viewer"
+    )
+
+    name = fields.Char()
+    field_type = fields.Char()
+
+
+class InventoryViewerAggregate(models.Model):
+    _name = "inventory.viewer.aggregate"
+
+    inventory_id = fields.Many2one(
+        "inventory.viewer"
+    )
+
+    field_name = fields.Char()
+    value = fields.Text()
