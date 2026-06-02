af41b3d1e05e21344830802e875a3190b75baded
Nuras1 <nurasbekmurad@mail.ru>
2026-06-02 14:52:26 +0500

    Add requests import

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM odoo:18
 
+RUN pip install requests
+
 COPY ./addons /mnt/extra-addons
 COPY ./odoo.conf /etc/odoo/odoo.conf
 
--- addons/inventory_viewer/models/import_wizard.py
+++ addons/inventory_viewer/models/import_wizard.py
@@ -1,13 +1,19 @@
 from odoo import models, fields
+import requests
 
 
 class InventoryImportWizard(models.TransientModel):
     _name = "inventory.import.wizard"
     _description = "Import Inventory"
 
-    api_url = fields.Char(
-        required=True
-    )
+    api_url = fields.Char(required=True)
 
     def action_import(self):
+
+        response = requests.get(self.api_url)
+
+        data = response.json()
+
+        print(data)
+
         return True
